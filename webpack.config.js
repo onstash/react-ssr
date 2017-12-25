d85ea97d1bffa80ae1a2df83f571c1a3ece454e9
Santosh Venkatraman <santosh.venk@gmail.com>
2017-12-25 22:30:47 +0530

    Fixes CleanWebpackPlugin() call

--- webpack.config.js
+++ webpack.config.js
@@ -47,7 +47,7 @@ module.exports = {
         ]
     },
     plugins: [
-        new CleanWebpackPlugin(),
+        new CleanWebpackPlugin(path.join(__dirname, "build")),
         new CompressionPlugin(),
         new AssetsPlugin({
           path: path.join(process.cwd(), 'build'),
